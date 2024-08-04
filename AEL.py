ace7a9407ffbd4c89494ebb50bf3af901ee86b84
Khang Nguyen <duykhang5687@gmail.com>
2024-08-04 18:04:17 +0700

    Update main class functions
    Add proper return statement to getChapters
    Add function to download chapters as PDF files, support threading

--- AEL.py
+++ AEL.py
@@ -1,14 +1,49 @@
+import os
 import re
 import requests
-from utils import load_cookie
+import threading
 from bs4 import BeautifulSoup
+from tqdm import tqdm
+from utils import load_cookie
 
 
 class AEL:
     def __init__(self):
-        self.url = "https://www.accessengineeringlibrary.com"
+        self.session = requests.Session()
 
-        # Validate cookies, if not valid, ask for login
+        # Update cookies and headers
+        cookies_dict = load_cookie()
+        self.session.cookies.update(cookies_dict)
+
+        # Headers to bypass "Access Denied" when trying to download certain chapters
+        self.session.headers.update(
+            {
+                "Host": "www.accessengineeringlibrary.com",
+                "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:128.0) Gecko/20100101 Firefox/128.0",
+                "Accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/png,image/svg+xml,*/*;q=0.8",
+                "Accept-Language": "en,en-US;q=0.5",
+                "Accept-Encoding": "gzip, deflate, br, zstd",
+                "Referer": "https://www.accessengineeringlibrary.com/",
+                "DNT": "1",
+                "Sec-GPC": "1",
+                "Connection": "keep-alive",
+                "Cookie": load_cookie(is_str=True),
+                "Upgrade-Insecure-Requests": "1",
+                "Sec-Fetch-Dest": "document",
+                "Sec-Fetch-Mode": "navigate",
+                "Sec-Fetch-Site": "cross-site",
+                "Sec-Fetch-User": "?1",
+            }
+        )
+        self.url = "https://www.accessengineeringlibrary.com"
+        self.chapters = []
+        self.chapter_links = []
+        # self.failed_downloads = {
+        #     "count": 0,
+        #     "chapter": [],
+        # }
+
+        # TODO: Validate cookies, if not valid, ask for login
 
         self.headers = {"Cookie": load_cookie()}
 
@@ -27,11 +62,12 @@ class AEL:
                 print("Did not reach response.get")
                 return None
 
-        # Change URL to /front-matter/preface1
+        # Remove all the subdirectories from the URL
+        # Example: https://www.accessengineeringlibrary.com/content/book/9780071793056
         url = re.sub(r"(/book/\d+).*", r"\1/", url)
-        url = url + "front-matter/preface1"
-
-        self.response = requests.get(url)
+        # Append /front-matter/preface1 to the url
+        placeholder_url = url + "front-matter/preface1"
+        self.response = self.session.get(placeholder_url)
         if self.response.status_code == 200:
             match = re.search(
                 r'name="f\[0\]" value="book_title:([^"]+)"', self.response.text
@@ -40,9 +76,13 @@ class AEL:
             return book_title
         return None
 
-    def getChapters(self):
+    def getChapters(self) -> str:
         """
-        This is a headache
+        This method retrieves the chapters of the book as a string for printing.
+        The actual HTML elements array can be accessed by self.chapters.
+
+        Returns:
+            str: The string representation of the chapters
         """
         soup = BeautifulSoup(self.response.text, "html.parser")
         toc_list = soup.find("ul", id="toc-list")
@@ -50,7 +90,7 @@ class AEL:
         # Find all <li> tags within this <ul>
         li_tags = toc_list.find_all("li")
 
-        self.chapters = []
+        # Find the chapter name and href links
         # Extract the text from <a> tags within <article> tags under each <li> tag
         for li in li_tags:
             article_tag = li.find("article", class_="toc-list-item")
@@ -60,20 +100,97 @@ class AEL:
                     href = a_tag["href"]
                     # Filter out sub-sections (href containing '#')
                     if not re.search(r"/book/\d+/.+?#", href):
-                        self.chapters.append(a_tag)
+                        # a_tag is an HTML element, so we can access its attributes like href
+                        # Get the chapter name
+                        chapter_name = "".join(
+                            [
+                                str(content).strip()
+                                for content in a_tag.contents
+                                if not content.name == "span"
+                            ]
+                        )
+                        # Page link is the URL + href attribute of the <a> tag
+                        chapter_page_link = self.url + href
 
-        # Print the Chapters
-        for chapter in self.chapters:
-            # Get all contents of the <a> tag excluding <span> tags
-            main_text = "".join(
-                [
-                    str(content).strip()
-                    for content in chapter.contents
-                    if not content.name == "span"
-                ]
-            )
-            return main_text
+                        # Chapter template to store the chapter name and page link
+                        # Later to be appended to self.chapters list
+                        chapter_template = {
+                            "name": chapter_name,
+                            "page_link": chapter_page_link,
+                        }
+                        self.chapters.append(chapter_template)
 
-    # TODO: Fetch download links from a_tags
+    def downloadChapters(self, use_threading=True):
+        """Download chapters via PDF links from self.chapters
 
-    # TODO: Download the chapters
+        Args:
+            use_threading (bool, optional): Enable threading to allow multiple download sessions. Defaults to True.
+        """
+        # Create a directory to store the downloaded files
+        if not os.path.exists("segments"):
+            os.makedirs("segments")
+
+        def download_chapter(chapter, index):
+            chapter_name = chapter["name"]
+            chapter_link = chapter["page_link"]
+
+            # Generate the file path to save the downloaded file with an index
+            file_path = f"segments/{index:03d}_{chapter_name}.pdf"
+
+            # Send a GET request to get PDF download link
+            # print("Sending request to {}".format(chapter_link))
+            response = self.session.get(chapter_link)
+            chapter_download_link = re.search(r'href="([^"]+\.pdf)"', response.text)
+
+            # Check if the chapter has a PDF download link
+            if chapter_download_link is None or response.status_code != 200:
+                print(f"Failed to find download link for {chapter_name}.pdf")
+                return
+
+            chapter_download_link = self.url + chapter_download_link.group(1)
+            # Send a GET request to download the PDF file with stream=True
+            response = self.session.get(chapter_download_link, stream=True)
+
+            # Check if the request was successful
+            if response.status_code == 200:
+                total_size = int(response.headers.get("content-length", 0))
+                block_size = 1024  # 1 Kibibyte
+                t = tqdm(
+                    total=total_size,
+                    unit="iB",
+                    unit_scale=True,
+                    desc=f"Downloading {chapter_name}.pdf",
+                )
+
+                # Save the downloaded file to the specified file path
+                with open(file_path, "wb") as file:
+                    for data in response.iter_content(block_size):
+                        t.update(len(data))
+                        file.write(data)
+                t.close()
+
+                if total_size != 0 and t.n != total_size:
+                    print(f"Failed to download {chapter_name}.pdf")
+                else:
+                    print(f"Downloaded {chapter_name}.pdf")
+            else:
+                print(f"Failed to download {chapter_name}.pdf")
+
+        # Download the chapters in order
+        if use_threading:
+            threads = []
+            for index, chapter in enumerate(self.chapters):
+                thread = threading.Thread(
+                    target=download_chapter, args=(chapter, index)
+                )
+                thread.start()
+                threads.append(thread)
+
+            # Wait for all threads to complete
+            for thread in threads:
+                thread.join()
+        else:
+            for index, chapter in enumerate(self.chapters):
+                download_chapter(chapter, index)
+
+    # TODO: Handle error when downloading chapters, i.e. PyPDF2.errors.PdfReadError: EOF marker not found which indicates that the cookie is invalid
