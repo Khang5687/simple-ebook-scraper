d83a76b6bd685e1d74c68d57e66673fbc269fe9a
Khang Nguyen <duykhang5687@gmail.com>
2024-08-20 23:56:37 +0700

    Strings for menu

---
+++ ascii_text.py
@@ -0,0 +1,14 @@
+title = """┏┓      ┏┓    •        •    ┓ •┓           ┳┓┓ 
+┣┫┏┏┏┓┏┏┣ ┏┓┏┓┓┏┓┏┓┏┓┏┓┓┏┓┏┓┃ ┓┣┓┏┓┏┓┏┓┓┏  ┃┃┃ 
+┛┗┗┗┗ ┛┛┗┛┛┗┗┫┗┛┗┗ ┗ ┛ ┗┛┗┗┫┗┛┗┗┛┛ ┗┻┛ ┗┫  ┻┛┗┛
+             ┛             ┛            ┛      """
+
+description = """Trước mắt thì chỉ hỗ trợ download sách từ AccessEngineeringLibrary thôi.
+Mình cần thêm feedback, nếu cần thêm trang web nào thì cứ nói cho mình.
+
+Github: https://github.com/Khang5687/simple-ebook-scraper
+Email: duykhang5687@gmail.com"""
+
+settings = """┏┓┏┓┏┳┓┏┳┓┳┳┓┏┓┏┓
+┗┓┣  ┃  ┃ ┃┃┃┃┓┗┓
+┗┛┗┛ ┻  ┻ ┻┛┗┗┛┗┛\nNothing works right now, come back in later versions"""
